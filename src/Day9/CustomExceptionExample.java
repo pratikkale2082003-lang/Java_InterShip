9af54765322dabb418811f3491f0e065d6d2e5b0
pratikkale2082003-lang <pratikkale2082003@gmail.com>
2026-07-01 18:24:23 +0530

    Initial commit

--- src/Day9/CustomExceptionExample.java
+++ src/Day9/CustomExceptionExample.java
@@ -1,5 +1,44 @@
 package Day9;
 
+
+//Create a custom exception called InvalidAgeException and use it in a program to validate age input.
+import java.util.Scanner;
+
+
+
+// Custom Exception
+class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
 public class CustomExceptionExample {
 
+    // Method to validate age
+    static void validateAge(int age) throws InvalidAgeException {
+
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above.");
+        } else {
+            System.out.println("Valid age. You are eligible.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        try {
+            validateAge(age);
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        sc.close();
+    }
 }
